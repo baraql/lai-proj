bee6707b73b6753848db6291b56e81ee56f1fb8d
Baraq Lipshitz <52468908+baraql@users.noreply.github.com>
2025-05-14 21:00:33 +0200

    Add files via upload

--- submit-llama3.sh
+++ submit-llama3.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=lsai
 #SBATCH --output=/iopsstor/scratch/cscs/baraq/proj/logs/%x-%j.out
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=72
 #SBATCH --mem=460000
@@ -20,7 +20,7 @@ ASSIGNMENT_DIR="/iopsstor/scratch/cscs/baraq/proj"
 
 CMD_PREFIX="numactl --membind=0-3"
 
-TRAINING_CMD="python3 $ASSIGNMENT_DIR/train.py \
+TRAINING_CMD="python3 $ASSIGNMENT_DIR/train_fsdp.py \
     --sequence-length 4096 \
     --batch-size 1 \
     --learning-rate 5e-5 \
@@ -28,15 +28,32 @@ TRAINING_CMD="python3 $ASSIGNMENT_DIR/train.py \
     --training-steps 1000 \
     "
 
+    
+
 PROFILING_CMD="nsys profile -s none -w true \
 --trace='nvtx,cudnn,cublas,cuda' \
 --output=/iopsstor/scratch/cscs/baraq/proj/nsys-trace.nsys-rep \
 --force-overwrite true \
 --capture-range=cudaProfilerApi \
---capture-range-end=stop -x true numactl --membind=0-3 python3 $ASSIGNMENT_DIR/train.py --profile"
-
-srun --cpus-per-task $SLURM_CPUS_PER_TASK bash -c "$CMD_PREFIX $TRAINING_CMD"
+--capture-range-end=stop -x true numactl --membind=0-3 python3 $ASSIGNMENT_DIR/train_fsdp.py --profile"
 
 # srun --cpus-per-task $SLURM_CPUS_PER_TASK bash -c "$PROFILING_CMD"
+# srun --cpus-per-task $SLURM_CPUS_PER_TASK bash -c "$CMD_PREFIX $TRAINING_CMD"
+
+export MASTER_ADDR=$(hostname)
+export MASTER_PORT=12355
+
+srun --cpus-per-task=$SLURM_CPUS_PER_TASK \
+  python3 -m torch.distributed.run \
+    --nnodes=1 \
+    --nproc_per_node=$SLURM_NTASKS_PER_NODE \
+    --rdzv_backend=c10d \
+    --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} \
+    $ASSIGNMENT_DIR/train_fsdp.py \
+      --sequence-length 4096 \
+      --batch-size 1 \
+      --learning-rate 5e-5 \
+      --lr-warmup-steps 100 \
+      --training-steps 1000
 
 echo "END TIME: $(date)"
